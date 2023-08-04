7565ba834a6cdb6877e5071b1585418299338155
Tharuka Kasthuriarachchige <tak@bth.se>
2023-08-04 18:36:11 +0200

    update

--- actors.py
+++ actors.py
@@ -38,25 +38,6 @@ class Rectangle:
 
         return not (left >= oright or right <= oleft or top >= obottom or bottom <= otop)
 
-
-class Lane(Rectangle):
-
-    def __init__(self, y, c=None, n=0, l=0, spc=0, spd=0):
-        super(Lane, self).__init__(
-            0, y * g_vars['grid'], g_vars['width'], g_vars['grid'])
-        self.type = t
-        self.color = c
-        self.obstacles = []
-        offset = random.uniform(0, 200)
-        if self.type == 'car':
-            o_color = (128, 128, 128)
-        if self.type == 'log':
-            o_color = (185, 122, 87)
-        for i in range(n):
-            self.obstacles.append(Obstacle(
-                offset + spc * i, y * g_vars['grid'], l * g_vars['grid'], g_vars['grid'], spd, o_color))
-
-
 class Frog(Rectangle):
 
     def __init__(self, x, y, w):
